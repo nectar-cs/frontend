106cede7e1482d306c1f50fb3de6315a9525c08f
xavier <xavier@codenectar.com>
2019-08-29 14:56:44 +0100

    basic tabs ui and ui logic working

--- src/modals/HttpActionsModal/HttpActionsModal.js
+++ src/modals/HttpActionsModal/HttpActionsModal.js
@@ -3,6 +3,7 @@ import s from './HttpActionsModal.sass'
 import {FULL_DEPLOYMENT} from "../../types/Deployment";
 import ModalButton from "../../widgets/Buttons/ModalButton";
 import LeftRightHeaders from "./LeftRightHeaders.js";
+import Tabs from "../../widgets/Tabs/Tabs";
 
 export default class HttpActionsModal extends React.Component {
   render(){
@@ -10,8 +11,13 @@ export default class HttpActionsModal extends React.Component {
       <div className={s.modal}>
         <LeftRightHeaders name={this.props.deployment.name}/>
         { this.renderRunButton() }
+        { this.renderTabs() }
       </div>
     )
+  }
+
+  renderTabs(){
+    return <Tabs tabs={['Destination', 'Source', 'Headers', 'Body']}/>
   }
 
   submit(){
---
+++ src/widgets/Tabs/Tabs.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import s from './Tabs.sass'
+
+export default class Tabs extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedInd: 0
+    };
+  }
+
+  render(){
+    return(
+      <div className={s.tabs}>
+        { this.props.tabs.map((t, i) => this.renderTab(t, i)) }
+      </div>
+    )
+  }
+
+  onTabSelected(index){
+    this.setState(s => ({...s, selectedInd: index}))
+  }
+
+  renderTab(tab, index){
+    const tabClass = index === this.state.selectedInd ? s.tabSel : s.tab;
+    return(
+      <p
+        key={index}
+        className={tabClass}
+        onClick={() => this.onTabSelected(index)}>
+        {tab}
+      </p>
+    )
+  }
+
+  static propTypes = {
+    tabs: PropTypes.arrayOf(PropTypes.string).isRequired
+  }
+
+}
